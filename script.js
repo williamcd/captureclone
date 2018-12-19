21d84ab059e33853f412cc6ee97e6b33c6b862f8
williamcd <wcdetrick@me.com>
2018-12-19 11:04:30 -0500

    fixed flickering issue, changed animation style to accomodate

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 $(document).ready(function () {
     $("#menu li").hover(function () {
-        $(".dropdown-list", this).delay(100).animate({ height: "toggle" })
+        $(".dropdown-list", this).clearQueue().delay(100).fadeIn()
     }, function () {
-        $(".dropdown-list", this).stop().delay(200).animate({ height: "toggle" })
-    })
-
-    $("#menu li").click(function() {
-        console.log(this.val)
+        $(".dropdown-list", this).stop().delay(200).fadeOut()
     })
 })
