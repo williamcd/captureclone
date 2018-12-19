ee4b162c06aba36f5f9d28b7ac70b42ce6302dc5
williamcd <wcdetrick@me.com>
2018-12-19 12:49:16 -0500

    got div toggles working via the nav bar. Thank you jQuery

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ $(document).ready(function () {
         $(".dropdown-list", this).stop().delay(200).fadeOut()
     })
 })
+
+const divArr = ["about", "login", "more1", "more2", "more3"]
+
+function toggle(x) {
+    var i;
+    for (i=0; i<divArr.length; i++) {
+        if (x === divArr[i]) {
+            $("#" + divArr[i]).css("display", "block")
+            console.log(divArr[i])
+        } else {
+            $("#" + divArr[i]).css("display", "none")
+        }
+    }
+}
