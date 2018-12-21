4fc03b8b687b345822e3b22b027e24e6c11a076e
williamcd <wcdetrick@me.com>
2018-12-21 12:49:19 -0500

    added basic animations to div views

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+    // set all divs to display: none; initially
+    // this allows each div to use flex, as the fadeIn method
+    // defaults to display: block;
+    const divArr = ["about", "login", "more1", "more2", "more3"]
+    var i;
+    for (i=0; i<divArr.length; i++) {
+        $("#" + divArr[i]).css("display", "none")
+    }
     // when hovering over a menu option
     $("#menu li").hover(function () {
         // check if that menu li has a child of .dropdown-list
@@ -20,14 +28,14 @@ function toggle(x) {
     for (i=0; i<divArr.length; i++) {
         // if input = an option, set that option's display to flex
         if (x === divArr[i]) {
-            $("#" + divArr[i]).css("display", "flex")
+            $("#" + divArr[i]).delay(100).fadeIn(100)
         // if the logo is clicked, input is "home", returns to home view
         } else if (x === "home") {
-            $("#" + divArr[i]).css("display", "none")
-            $("#home").css("display", "block")
+            $("#" + divArr[i]).fadeOut(100)
+            $("#home").delay(100).fadeIn(100)
         // all options that != input are set to display: none;
         } else {
-            $("#" + divArr[i]).css("display", "none")
+            $("#" + divArr[i]).fadeOut(100)
         }
     }
 }
