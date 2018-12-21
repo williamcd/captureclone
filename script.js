1c75b4d52d80581651f8b5d4e295d3dedf763234
williamcd <wcdetrick@me.com>
2018-12-21 14:35:47 -0500

    changed menu animations/spacing and added Capio logos as Capture logo placeholder

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // set all divs (except home) to display: none; initially.
     // this allows each div to use flex, as the fadeIn method defaults to display: block;
-    const divArr = ["about", "login", "more1", "more2", "more3"]
+    const divArr = ["login", "more1", "more2", "more3"]
     var i;
     for (i=0; i<divArr.length; i++) {
         $("#" + divArr[i]).css("display", "none")
@@ -28,11 +28,7 @@ function toggle(x) {
         // if input = an option, set that option's display to flex
         if (x === divArr[i]) {
             $("#" + divArr[i]).delay(100).fadeIn(100)
-        // if the logo is clicked, input is "home", returns to home view
-        } else if (x === "home") {
-            $("#" + divArr[i]).fadeOut(100)
-            $("#home").delay(100).fadeIn(100)
-        // all options that != input are set to display: none;
+        // all non-matching options are faded out
         } else {
             $("#" + divArr[i]).fadeOut(100)
         }
